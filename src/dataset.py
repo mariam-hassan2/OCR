cbf8214dde0e6a48e59dd134c7fc0cb900480755
mariam-hassan2 <121635911+mariam-hassan2@users.noreply.github.com>
2026-03-14 04:13:16 -0400

    Create dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from torch.utils.data import Dataset
+from PIL import Image
+from .utils import preprocess_image
+from transformers import TrOCRProcessor
+
+class LicenseDataset(Dataset):
+    def __init__(self, folder_path, processor, augment=False):
+        self.df = build_df(folder_path)  # Your build_df
+        self.processor = processor
+        self.augment = augment
+    
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        image = Image.open(row['image_path']).convert('RGB')
+        image = preprocess_image(image)  # Your PIL
+        
+        pixels = self.processor(image, return_tensors="pt").pixel_values.squeeze()
+        tokens = self.processor.tokenizer(row['label'], padding="max_length", max_length=20).input_ids
+        labels = torch.tensor([t if t != self.processor.tokenizer.pad_token_id else -100 for t in tokens])
+        return {"pixel_values": pixels, "labels": labels}
